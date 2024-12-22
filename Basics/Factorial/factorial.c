d8455f64042d10b7d72fd7230be915d6fed98de2
Amit Anand Jha <amitanandjha12g@gmail.com>
2024-12-22 23:59:33 +0530

    Add files via upload

--- Basics/Factorial/factorial.c
+++ Basics/Factorial/factorial.c
@@ -1,1 +1,41 @@
+#include<stdio.h>
 
+int main()
+{
+	int n,val;
+
+	printf("\n\nNamaste !\n\nThis code will print Factorial Value of the number you will enter below\n\n");
+	
+	printf("\n\nPlease enter the number\n\n");
+	
+	scanf("%d",&n);
+	
+	if(n<0)
+	{
+		printf("\n\nFactorial value does not exist for negative numbers\n\n");
+	}
+	else if(n==0)
+	{
+		printf("\n\nWell, weirldy 0!=1\n\n");
+	}
+	else
+	{
+		printf("\n\nThe value of factorial of %d is\n",n);
+	
+		val=factorial(n);
+	}
+	return 0;
+}
+
+int factorial(int n)
+{	
+	int k,count=n;
+	while(count!=0)
+	{
+		k=n*factorial(n-1);
+		count=count-1;
+	}
+	printf("\n%d,k");
+
+	return k;
+}
